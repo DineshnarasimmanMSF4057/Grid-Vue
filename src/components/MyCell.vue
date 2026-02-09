6b7abb87af1870f9885fcb049018d1bd6cf8dced
DineshnarasimmanMSF4057 <125851531+DineshnarasimmanMSF4057@users.noreply.github.com>
2026-02-09 18:22:05 +0530

    Added

---
+++ src/components/MyCell.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+ <button class="e-btn e-primary" >OK</button>
+  </div>
+</template>
+
+<script>
+
+
+
+
+import {
+  ButtonComponent,
+
+} from '@syncfusion/ej2-vue-buttons';
+
+
+export default {
+  components: {
+
+    'ejs-button': ButtonComponent,
+
+  },
+
+  props: {
+    rowData: {
+   type: Object,
+   required: true,
+ },
+},
+  data() {
+    
+    return {
+     
+    };
+  },
+  methods: {
+    btnClick: function () {
+      alert()
+    },
+  },
+};
+</script>
